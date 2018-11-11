e8b92dd712747b66401b2c6d3067c42b4e51d078
na-be-ma <natebemartin@gmail.com>
2018-11-11 11:16:21 -0500

    Add files via upload

--- functions.js
+++ functions.js
@@ -1,23 +1,47 @@
-var player_1 = new Array();
-var player_2 = new Array();
-
-var xandy = 0;
+//stores all clicked id's
+var wasClicked = [];
+//stores each ind. player's id
+var player_1 = []; 
+var player_2 = [];
+//checks if 'p' contains a winning array
+function checkWin(p) {
+    
+    var combos = [
+        ["a1", "a2", "a3"], ["b1", "b2", "b3"], ["c1", "c2", "c3"], 
+        ["a1", "b1", "c1"], ["a2", "b2", "c2"], ["a3", "b3", "c3"], 
+        ["a1", "b2", "c3"], ["a3", "b2", "c1"]
+    ];
+      
+    for(i = 0; i < combos.length; i++) {
+        
+        if(p.includes(combos[i][0]) && p.includes(combos[i][1]) && p.includes(combos[i][2]))
+            return true;
+    }
+    
+    return false;
+}
+//checks if 'event.target.id' is already contained in player array
+function checkValid(array, p){
+    if(!array.includes(p)){ console.log("true"); return true; } else { console.log("false"); return false; }
+}
 
 // invoked whenever a td element is clicked on; event.target refers to which td was clicked on
 function clicked(event) {
-    if(xandy%2==0){
-        event.target.setAttribute("class", "red");
-        xandy++;
-        
-        player_1 = HTMLElementObject.className;
+    if(wasClicked.length%2==0){     
+        //if(statement) returns true, change tile color, add 'id' to 
+        if(checkValid(wasClicked, event.target.id)) { player_1.push(event.target.id); console.log(player_1); wasClicked.push(event.target.id); event.target.setAttribute("class", "red"); 
+            if(checkWin(player_1) ) { console.log("Player 1 Wins!"); }                                                    
+        }             
     }
     else {
-        event.target.setAttribute("class", "blue");
-        xandy++;
-    }
     
-    if(xandy == 9) {
-        location.reload();
+        if(checkValid(wasClicked, event.target.id)) { player_2.push(event.target.id); console.log(player_2); wasClicked.push(event.target.id); event.target.setAttribute("class", "blue"); 
+            if(checkWin(player_2) ) { console.log("Player 2 Wins!"); }                                                     
+        }       
+    }
+    if(wasClicked.length==9){
+        window.location.reload();
+        console.log("It's a draw");
     }
 }
 
