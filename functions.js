c4f93add244b3d5c8e3c435a5cb0739cde7efe90
na-be-ma <natebemartin@gmail.com>
2018-12-01 22:29:25 -0500

    Add files via upload
    2nd time best time

--- functions.js
+++ functions.js
@@ -1,58 +1,68 @@
-//stores all clicked id's
-var wasClicked = [];
-//stores each ind. player's id
-var player_1 = []; 
+document.addEventListener("DOMContentLoaded", setup);
+
+var player_1 = [];
 var player_2 = [];
-//checks if 'p' contains a winning array
-function checkWin(p) {
-    
-    var combos = [
-        ["a1", "a2", "a3"], ["b1", "b2", "b3"], ["c1", "c2", "c3"], 
-        ["a1", "b1", "c1"], ["a2", "b2", "c2"], ["a3", "b3", "c3"], 
-        ["a1", "b2", "c3"], ["a3", "b2", "c1"]
-    ];
-      
-    for(i = 0; i < combos.length; i++) {
-        
-        if(p.includes(combos[i][0]) && p.includes(combos[i][1]) && p.includes(combos[i][2]))
-            return true;
-    }
-    
-    return false;
-}
-//checks if 'event.target.id' is already contained in player array
-function checkValid(array, p){
-    if(!array.includes(p)){ console.log("true"); return true; } else { console.log("false"); return false; }
-}
 
-// invoked whenever a td element is clicked on; event.target refers to which td was clicked on
-function clicked(event) {
-    if(wasClicked.length%2==0){     
-        //if(statement) returns true, change tile color, add 'id' to 
-        if(checkValid(wasClicked, event.target.id)) { player_1.push(event.target.id); console.log(player_1); wasClicked.push(event.target.id); event.target.setAttribute("class", "red"); 
-            if(checkWin(player_1) ) { console.log("Player 1 Wins!"); }                                                    
-        }             
-    }
-    else {
-    
-        if(checkValid(wasClicked, event.target.id)) { player_2.push(event.target.id); console.log(player_2); wasClicked.push(event.target.id); event.target.setAttribute("class", "blue"); 
-            if(checkWin(player_2) ) { console.log("Player 2 Wins!"); }                                                     
-        }       
-    }
-    if(wasClicked.length==9){
-        window.location.reload();
-        console.log("It's a draw");
-    }
-}
+var wasClicked = [];
 
-// sets up 9 onclick event listeners, one for each td element
-function setup() {
+function setup(e) {
+    console.log(e);
     var cells = document.getElementsByTagName("td");
-    
-    for(var i = 0; i < cells.length; i++) {
+    for(i = 0; i < cells.length; i++) {
         cells[i].addEventListener("click", clicked);
     }
 }
 
-// Wait for the html to be read entirely first, then run "setup"
-document.addEventListener("DOMContentLoaded", setup);
+function clicked() {
+    console.log(event.target.id);
+    
+    if(!wasClicked.includes(event.target.id)){
+
+            player_1.push(event.target.id);
+            wasClicked.push(event.target.id);
+            event.target.classList.add("blue");
+            if(hasWin(player_1)){
+                document.body.classList.add("black")
+            }
+            console.log(player_1, wasClicked);
+            autoPilot(wasClicked);
+        }
+        
+    
+}
+
+function autoPilot(x) {
+    var tab = document.getElementsByClassName("square");
+    var random = tab[Math.floor(Math.random() * 9)];
+    /*
+    */
+        console.log(random.id);
+        if(x.includes(random.id)){
+            autoPilot(wasClicked);
+        } 
+        else{
+            //getRando().classList.add("red");
+            random.classList.add("red");
+            player_2.push(random.id);
+            wasClicked.push(random.id);
+            console.log(player_2, wasClicked);
+                        
+            if(hasWin(player_2)){
+                document.body.classList.add("black");
+                console.log("Winner2");
+            }
+        } 
+ }
+
+function hasWin(x) {
+    var winner = [["a1", "a2", "a3"], ["b1", "b2", "b3"], ["c1","c2","c3"], ["a1", "b1", "c1"],
+                  ["a2","b2","c2"], ["a3","b3","c3"], ["a1", "b2", "c3"], ["a3", "b2", "c1"]];
+    for(i = 0; i < winner.length; i++){
+        //console.log(winner[i]);
+        if(x.includes(winner[i][0]) && x.includes(winner[i][1]) && x.includes(winner[i][2])){
+            console.log("win!!!");
+            return true;
+        }
+}  
+        
+}
